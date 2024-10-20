c31231438e787a1e8cb5511954dfb03a3398286e
CamiloP4ez <159430746+CamiloP4ez@users.noreply.github.com>
2024-10-20 14:34:13 +0100

    Refactor: profileInfo
    refactoring and creating profile info component

---
+++ components/ProfileInfo.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+const ProfileInfo = () => {
+  return (
+    <View style={styles.profileContainer}>
+      <Image
+        style={styles.avatar}
+        source={require("../assets/SofyanAmrabat.jpg")}
+      />
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.title}>Descripción sobre mí!</Text>
+        <Text>
+          Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar
+          prácticas para mis queridos alumnos
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  avatar: {
+    height: 90,
+    width: 90,
+    borderRadius: 100,
+  },
+  descriptionContainer: {
+    margin: 10,
+    backgroundColor: "lightgray",
+    padding: 10,
+    borderRadius: 10,
+    width: "70%",
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 20,
+  },
+});
+
+export default ProfileInfo;
